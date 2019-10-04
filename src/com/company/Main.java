ecf71ad5825a9f11893b204127a73a71693b10e8
Tarabra_github <ragnar.tir@yandex.ru>
2019-10-04 12:19:11 +0300

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Second Commit");
         651265165165
     }
 }
