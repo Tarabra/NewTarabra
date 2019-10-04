6efdeee5c76f30eb79a94fb490b992c8b0aa76fe
Tarabra_github <ragnar.tir@yandex.ru>
2019-10-04 12:30:56 +0300

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Second Commit");
 	System.out.println("Changes on IDE Intellij Idea");
-        651265165165
+        00000000000000000
     }
 }
